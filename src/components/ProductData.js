431ce9599f89d7ea21e69052e77467da225395ea
Ruckykel <ruckykel1@gmail.com>
2025-01-02 22:29:37 +0100

    nfue

--- src/components/ProductData.js
+++ src/components/ProductData.js
@@ -1,259 +1,259 @@
 export const ProductData = [
     {
       id: 1,
-      name: "Classic White Shirt",
+      name: "Classic White Glass Suit",
       price: 45000,
       mainImage: "/images/One1.jpg",
       hoverImage: "/images/One2.jpg"
     },
     {
       id: 2,
-      name: "Black Denim Jeans",
+      name: "Black Beaded Suit",
       price: 65000,
       mainImage: "/images/Two1.jpg",
       hoverImage: "/images/Two2.jpg"
     },
     {
       id: 3,
-      name: "Floral Summer Dress",
+      name: "Patterned PE Suit",
       price: 55000,
       mainImage: "/images/Three1.jpg",
       hoverImage: "/images/Three2.jpg"
     },
     {
         id: 4,
-        name: "Floral Summer Dress",
+        name: "Classic White Glass Suit",
         price: 55000,
         mainImage: "/images/Four1.jpg",
         hoverImage: "/images/Four2.jpg"
       },
       {
         id: 5,
-        name: "Floral Summer Dress",
+        name: "Patterned PE Suit",
         price: 55000,
         mainImage: "/images/Five1.jpg",
         hoverImage: "/images/Five2.jpg"
       },
       {
         id: 6,
-        name: "Floral Summer Dress",
+        name: "Black Mesh Extended Bow Shirt",
         price: 55000,
         mainImage: "/images/Six1.jpg",
         hoverImage: "/images/Six2.jpg"
       },
       {
         id: 7,
-        name: "Floral Summer Dress",
+        name: "Custom Beaded Suit",
         price: 55000,
         mainImage: "/images/Seven1.jpg",
         hoverImage: "/images/Seven2.jpg"
       },
       {
         id: 8,
-        name: "Floral Summer Dress",
+        name: "Custom Beaded Suit",
         price: 55000,
         mainImage: "/images/Eight1.jpg",
         hoverImage: "/images/Eight2.jpg"
       },
       {
         id: 9,
-        name: "Floral Summer Dress",
+        name: "Custom PE Signature",
         price: 55000,
         mainImage: "/images/Nine1.jpg",
         hoverImage: "/images/Nine2.jpg"
       },
       {
         id: 10,
-        name: "Floral Summer Dress",
+        name: "2-Piece Grey Suit",
         price: 55000,
         mainImage: "/images/Ten1.jpg",
         hoverImage: "/images/Ten2.jpg"
       },
       {
         id: 11,
-        name: "Floral Summer Dress",
+        name: "Custom Beaded Tuxedo",
         price: 55000,
         mainImage: "/images/Eleven1.jpg",
         hoverImage: "/images/Eleven2.jpg"
       },
       {
         id: 12,
-        name: "Floral Summer Dress",
+        name: "Amund Damask Two Piece",
         price: 55000,
         mainImage: "/images/Twelve1.jpg",
         hoverImage: "/images/Twelve2.jpg"
       },
       {
         id: 13,
-        name: "Floral Summer Dress",
+        name: "Amund Danshiki",
         price: 55000,
         mainImage: "/images/Thirteen1.jpg",
         hoverImage: "/images/Thirteen2.jpg"
       },
       {
         id: 14,
-        name: "Floral Summer Dress",
+        name: "Yinka Oversize Two Piece",
         price: 55000,
         mainImage: "/images/Fourteen1.jpg",
         hoverImage: "/images/Fourteen2.jpg"
       },
       {
         id: 15,
-        name: "Floral Summer Dress",
+        name: "Custom Beaded Tuxedo",
         price: 55000,
         mainImage: "/images/Fifteen1.jpg",
         hoverImage: "/images/Fifteen2.jpg"
       },
       {
         id: 16,
-        name: "Floral Summer Dress",
+        name: "Two Piece Summer set",
         price: 55000,
         mainImage: "/images/Sixteen1.jpg",
         hoverImage: "/images/Sixteen2.jpg"
       },
       {
         id: 17,
-        name: "Floral Summer Dress",
+        name: "Two Piece Summer Set",
         price: 55000,
         mainImage: "/images/Seventeen1.jpg",
         hoverImage: "/images/Seventeen2.jpg"
       },
       {
         id: 18,
-        name: "Floral Summer Dress",
+        name: "Segun Two Piece",
         price: 55000,
         mainImage: "/images/Eighteen1.jpg",
         hoverImage: "/images/Eighteen2.jpg"
       },
       {
         id: 19,
-        name: "Floral Summer Dress",
+        name: "Nnaji Signature",
         price: 55000,
         mainImage: "/images/Nineteen1.jpg",
         hoverImage: "/images/Nineteen2.jpg"
       },
       {
         id: 20,
-        name: "Floral Summer Dress",
+        name: "Collins Wander",
         price: 55000,
         mainImage: "/images/Twenty1.jpg",
         hoverImage: "/images/Twenty2.jpg"
       },
       {
         id: 21,
-        name: "Floral Summer Dress",
+        name: "Infinity Two Piece Set",
         price: 55000,
         mainImage: "/images/TwentyOne1.jpg",
         hoverImage: "/images/TwentyOne2.jpg"
       },
       {
         id: 22,
-        name: "Floral Summer Dress",
+        name: "Segun Arc Design Kaftan",
         price: 55000,
         mainImage: "/images/TwentyTwo1.jpg",
         hoverImage: "/images/TwentyTwo2.jpg"
       },
       {
         id: 23,
-        name: "Floral Summer Dress",
+        name: "Abstract Two Piece Suit",
         price: 55000,
         mainImage: "/images/TwentyThree1.jpg",
         hoverImage: "/images/TwentyThree2.jpg"
       },
       {
         id: 24,
-        name: "Floral Summer Dress",
+        name: "Psalmy Safari Set",
         price: 55000,
         mainImage: "/images/TwentyFour1.jpg",
         hoverImage: "/images/TwentyFour2.jpg"
       },
       {
         id: 25,
-        name: "Floral Summer Dress",
+        name: "Dito Sleeveless Shirt",
         price: 55000,
         mainImage: "/images/TwentyFive1.jpg",
         hoverImage: "/images/TwentyFive2.jpg"
       },
       {
         id: 26,
-        name: "Floral Summer Dress",
+        name: "Bold Mismatch Three Piece Suit",
         price: 55000,
         mainImage: "/images/TwentySix1.jpg",
         hoverImage: "/images/TwentySix2.jpg"
       },
       {
         id: 27,
-        name: "Floral Summer Dress",
+        name: "Two Piece Pixel Suit",
         price: 55000,
         mainImage: "/images/TwentySeven1.jpg",
         hoverImage: "/images/TwentySeven2.jpg"
       },
       {
         id: 28,
-        name: "Floral Summer Dress",
+        name: "Single Lapel Two Piece",
         price: 55000,
         mainImage: "/images/TwentyEight1.jpg",
         hoverImage: "/images/TwentyEight2.jpg"
       },
       {
         id: 29,
-        name: "Floral Summer Dress",
+        name: "Men’s Wool Camel Coat With Fox Fur",
         price: 55000,
         mainImage: "/images/TwentyNine1.jpg",
         hoverImage: "/images/TwentyNine2.jpg"
       },
       {
         id: 30,
-        name: "Floral Summer Dress",
+        name: "Men’s Gothic Court Prince Blazer",
         price: 55000,
         mainImage: "/images/Thirty1.jpg",
         hoverImage: "/images/Thirty2.jpg"
       },
       {
         id: 31,
-        name: "Floral Summer Dress",
+        name: "Men’s Velvet Jacket",
         price: 55000,
         mainImage: "/images/ThirtyOne1.jpg",
         hoverImage: "/images/ThirtyTwo2.jpg"
       },
       {
         id: 32,
-        name: "Floral Summer Dress",
+        name: "Segun’s Kaftan Set",
         price: 55000,
         mainImage: "/images/ThirtyTwo1.jpg",
         hoverImage: "/images/ThirtyTwo2.jpg"
       },
       {
         id: 33,
-        name: "Floral Summer Dress",
+        name: "Amund Oversize Kaftan",
         price: 55000,
         mainImage: "/images/ThirtyThree1.jpg",
         hoverImage: "/images/ThirtyThree2.jpg"
       },
       {
         id: 34,
-        name: "Floral Summer Dress",
+        name: "Femi Kaftan",
         price: 55000,
         mainImage: "/images/ThirtyFour1.jpg",
         hoverImage: "/images/ThirtyFour2.jpg"
       },
       {
         id: 35,
-        name: "Floral Summer Dress",
+        name: "Amund Two Piece Kaftan",
         price: 55000,
         mainImage: "/images/ThirtyFive1.jpg",
         hoverImage: "/images/ThirtyFive2.jpg"
       },
       {
         id: 36,
-        name: "Floral Summer Dress",
+        name: "Twin Set",
         price: 55000,
         mainImage: "/images/ThirtySix1.jpg",
         hoverImage: "/images/ThirtySix2.jpg"
       },
       {
         id: 37,
-        name: "Floral Summer Dress",
+        name: "Amund Neckline Kaftan",
         price: 55000,
         mainImage: "/images/ThirtySeven1.jpg",
         hoverImage: "/images/ThirtySeven2.jpg"
